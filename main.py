1ca84ca4eea9343cdaf102619e20424f39340126
stil3t <galliy7rezerford@gmail.com>
2021-04-11 23:43:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def main(file):
 	line_number = 0
 
 	for line in input_file:
-		for lit in re.findall(r'[\']\w+[\']', line)+re.findall(r'[\"]\w+[\"]', line):
+		for lit in re.findall(r'[\']\w+[\']', line) + re.findall(r'[\"]\w+[\"]', line):
+
 			lit = lit[1:-1]
 
 			if not lit in lits.keys():
@@ -26,10 +27,10 @@ def main(file):
 				if str(line_number) not in lits[lit]:
 					lits[lit].append(str(line_number))
 		
-		line_number+=1
+		line_number += 1
 
 	for i in lits.keys():
-		if len(lits[i])>1:
+		if len(lits[i]) > 1:
 			print('Lines with \'{0}\': {1}'.format(i, ', '.join(lits[i])))
 
 	input_file.close()
